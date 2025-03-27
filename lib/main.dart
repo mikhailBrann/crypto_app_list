389a304fb3fc57f84ce669db44f7819b10907f5f
Mikhail Brann <alunos13@yandex.com>
2025-03-27 15:45:50 +0300

    main added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,17 @@ import 'package:crypto_app_list/repositories/crypto_coins/crypto_coins.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 
-void main() {
+void main() async {
+  // инициализация firebase
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   // регистратор объектов глобальной видимости
   // регистрируем через синглтон для того что бы на все приложение отрабатывал только один экземпляр объекта
   // lazySinglton - будет создан только при первом обращении к нему
